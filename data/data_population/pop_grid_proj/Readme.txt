4d18c873c82c00d665c67ca12dea22d25abbe699
MartaMastropietro <martamastro17@gmail.com>
2026-02-03 17:28:29 +0100

    added pop data isimip

--- data/data_population/pop_grid_proj/Readme.txt
+++ data/data_population/pop_grid_proj/Readme.txt
@@ -4,4 +4,5 @@ DOI		https://doi.org/10.6084/m9.figshare.19608594
 Data Last Update	8/29/2022
 Authors		Xinyu Wang & Xiangfeng Meng, Ying Long
 
-This dataset is a global gridded population dataset covering 248 countries or areas at 30 arc-seconds (approximately 1 km) spatial resolution with five-year intervals for the period 2020–2100 by implementing random forest (RF) algorithm. It is quantitatively consistent with the shared socioeconomic pathways (SSPs) national population. The grids are stored in five folders in GEOTIFF format.
+This dataset is a global gridded population dataset covering 248 countries or areas at 30 arc-seconds (approximately 1 km) spatial resolution with five-year intervals for the period 2020–2100 by implementing random forest (RF) algorithm. 
+It is quantitatively consistent with the shared socioeconomic pathways (SSPs) national population. The grids are stored in five folders in GEOTIFF format.
--- utils/libraries.R
+++ utils/libraries.R
@@ -1,20 +1,17 @@
 
 ### Libraries
 
-# install.packages( c( "arrow", "raster", "terra", "sp", "sf",
-#   "ncdf4","ncdf4.helpers","readr","dplyr","tidyr","ggplot2","tidyverse",
-#   "REdaS","progress","reshape2","lattice","readxl","plm","splm",
-#   "spdep","car","exactextractr","data.table","clubSandwich",
-#   "stargazer","gsubfn","margins","fixest","modelsummary")
-# )
+# install.packages(c(
+# "broom","arrow","raster","terra","sp","sf","ncdf4","ncdf4.helpers","PCICt",
+# "readr","dplyr","tidyr","ggplot2","tidyverse","REdaS","progress","reshape","reshape2",
+# "lattice","readxl","car","exactextractr","data.table","clubSandwich","stargazer",
+# "gsubfn","margins","fixest","modelsummary"))
 
 
 
 library(broom)
-
 library(arrow)
 library(raster)
-#library(rnaturalearth)
 library(terra)
 library(sp)
 library(sf)
@@ -30,28 +27,15 @@ library(REdaS)
 library(progress)
 library(reshape)
 library(reshape2)
-# library(wbstats)
-# library(XICOR)
-# library(mice)
-# library(VIM)
 library(lattice)
 library(readxl)
-# library(gdalUtils)
 library(plm)
-# library(splm)
-# library(spdep)
 library(car)
-
 library(exactextractr)
 library(data.table)
-
 library(clubSandwich)
 library(stargazer)
 library(gsubfn)
 library(margins)
-
-# library(jtools)
-
 library(fixest)
-
 library(modelsummary)
